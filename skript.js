964f25f443eb29f4db46a667661c431e161b8162
IKot12 <ilkot1254@outlook.com>
2024-03-27 15:40:13 +0300

    hw5

--- skript.js
+++ skript.js
@@ -1,61 +1,122 @@
+// //1
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+
+// //2
+// const iPone = 2007;
+// alert(iPone);
+
+// //3
+// const mAsterjs = "BrendanEich";
+// alert(mAsterjs);
+
+// //4
+// let x = 10, y = 2;
+// alert(x - y);
+// alert(x + y);
+// alert(x * y);
+// alert(x / y);
+
+// //5
+// const result = 2 ** 5;
+// alert(result);
+
+// //6
+// let b = 9%2;
+// alert(b);
+
+// //7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num ++;
+// num --;
+// alert(num);
+
+// //8
+// let age = prompt('Сколько вам лет?');
+// alert(age);
+
+// //9
+// let user = {
+//     name : 'Victor',
+//     age : 27,
+//     isAdmin: true
+// };
+
+// user = "New York";
+
+// user.age = 35;
+
+// delete user["city of residence"];
+
+// let info = ["Какую информацию хотите узнать о пользователе?"];
+// alert(user[info]);
+
+// let userName = prompt ("Как вас зовут?");
+// alert (`Привет, ${userName}!`);
+
+//2.5
+
 //1
-let a = 10;
-alert(a);
-a = 20;
-alert(a);
+
+// function number(x, y) {
+//     if (x > y) {
+//         return y
+//     }
+//     if (x < y) {
+//         return x
+//     }
+// }
+
+// console.log(number(4, 8));
 
 //2
-const iPone = 2007;
-alert(iPone);
+
+// function sayHello(a) {
+
+//     if (a % 2 === 0) {
+//         return 'Чило четное'
+//     } else {
+//         return 'Число не четное'
+//     }
+// }
+
+// console.log(sayHello(5));
 
 //3
-const mAsterjs = "BrendanEich";
-alert(mAsterjs);
+
+//3.1
+// function youNumbers(num) {
+//     console.log(num * num);
+// }
+// //3.2
+// function getNumbers(num) {
+//     return (num * num);
+// }
 
 //4
-let x = 10, y = 2;
-alert(x - y);
-alert(x + y);
-alert(x * y);
-alert(x / y);
+
+// function youAge() {
+
+//     const ageUser = Number(prompt('Введите свой возраст'));
+
+//     if (ageUser >= 0 && ageUser <= 12  ) {
+//         alert('Привет друг')
+//     }
+//     if (ageUser >= 13) {
+//         alert('Добро пожаловать')
+//     }
+//     if (ageUser < 0) {
+//         alert('Вы ввели неправильное значение')
+//     }
+// }
+
+// youAge();
 
 //5
-const result = 2 ** 5;
-alert(result);
 
-//6
-let b = 9%2;
-alert(b);
-
-//7
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num ++;
-num --;
-alert(num);
-
-//8
-let age = prompt('Сколько вам лет?');
-alert(age);
-
-//9
-let user = {
-    name : 'Victor',
-    age : 27,
-    isAdmin: true
-};
-
-user = "New York";
-
-user.age = 35;
-
-delete user["city of residence"];
-
-let info = ["Какую информацию хотите узнать о пользователе?"];
-alert(user[info]);
-
-let userName = prompt ("Как вас зовут?");
-alert (`Привет, ${userName}!`);
